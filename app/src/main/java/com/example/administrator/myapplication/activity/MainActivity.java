dbf9104e44448159fbb21458a20c2e0419700410
Potatos45 <2561024377@qq.com>
2017-06-29 22:28:09 +0800

    点击随机测试进入考试页面

--- app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.myapplication.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 import com.example.administrator.myapplication.R;
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
+    }
+
+    public void test(View view) {
+
+startActivity(new Intent(MainActivity.this,ExamActivity.class));
+    }
+
+    public void exit(View view) {
+        finish();
     }
 }
