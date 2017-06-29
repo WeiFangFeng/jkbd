8522cd1ce2c6fdd5b0c112dd2714ccc204090b1c
Potatos45 <2561024377@qq.com>
2017-06-29 17:01:08 +0800

    考卷信息的toString方法

--- app/src/main/java/com/example/administrator/myapplication/bean/ExamInfo.java
+++ app/src/main/java/com/example/administrator/myapplication/bean/ExamInfo.java
@@ -49,4 +49,11 @@ public class ExamInfo {
     public void setQuestionCount(int questionCount) {
         this.questionCount = questionCount;
     }
+
+    @Override
+    public String toString() {
+        return "考试科目：" + subjectTitle + "\n"+
+                ", 考题数量：" + questionCount +"\n"+
+                ",考试时间：" + limitTime +"分钟";
+    }
 }
