2ad35c5fb840ebe66e55a296de15ce82267ed903
Potatos45 <2561024377@qq.com>
2017-06-30 11:31:08 +0800

    MainActivityd的修改

--- app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.administrator.myapplication.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 
 
 import com.example.administrator.myapplication.R;
+import com.example.administrator.myapplication.bean.ExamInfo;
+import com.example.administrator.myapplication.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void test(View view) {
+        OkHttpUtils<EditorInfo>utils= new OkHttpUtils<>(getApplication());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+
+        /*utils.url(uri).targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<EditorInfo>() {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });*/
       startActivity(new Intent(MainActivity.this,ExamActivity.class));
 
     }
