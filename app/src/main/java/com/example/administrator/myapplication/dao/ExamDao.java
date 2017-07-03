9f825f402b6ff910fe48e52c602e7692b9b7d671
Potatos45 <2561024377@qq.com>
2017-07-03 19:32:09 +0800

    添加dao包

---
+++ app/src/main/java/com/example/administrator/myapplication/dao/ExamDao.java
@@ -0,0 +1,73 @@
+package com.example.administrator.myapplication.dao;
+
+import android.util.Log;
+
+import com.example.administrator.myapplication.ExamApplication;
+import com.example.administrator.myapplication.activity.ExamActivity;
+import com.example.administrator.myapplication.bean.Exam;
+import com.example.administrator.myapplication.bean.ExamInfo;
+import com.example.administrator.myapplication.bean.Result;
+import com.example.administrator.myapplication.utils.OkHttpUtils;
+import com.example.administrator.myapplication.utils.ResultUtils;
+
+import java.util.List;
+
+/**
+ * Created by weifangfeng on 2017/7/2.
+ */
+
+public class ExamDao implements IExamDao {
+    @Override
+    public void loadExamInfo() {
+        OkHttpUtils<ExamInfo> utils= new OkHttpUtils<>(ExamApplication.getInstance());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
+
+        utils.url(uri).targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                        ExamApplication.getInstance().setMExamInfo(result);
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
+
+    }
+
+    @Override
+    public void loadQusetionLists() {
+
+        OkHttpUtils<String>utils1=new OkHttpUtils<String>(ExamApplication.getInstance());
+        String url2="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand\n";
+        utils1.url(url2)
+                .targetClass(String.class)
+                .execute(new OkHttpUtils.OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String jsonStr) {
+                        Result result= ResultUtils.getListResultFromJson(jsonStr);
+                        if(result!=null && result.getError_code()==0){
+                            List<Exam> list=result.getResult();
+                            if(list!=null && list .size()>0){
+                                ExamApplication.getInstance().setMExamList(list);
+
+                            }
+                        }
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/dao/IExamDao.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myapplication.dao;
+
+/**
+ * Created by weifangfeng on 2017/7/2.
+ */
+
+public interface IExamDao {
+    void loadExamInfo();
+    void loadQusetionLists();
+}
