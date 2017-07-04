53021943bac3dca3a0b87e39d6587442d1b01100
Potatos45 <2561024377@qq.com>
2017-07-04 21:47:24 +0800

    设置计算分数，但分值计算有不懂

--- app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
@@ -1,10 +1,13 @@
 package com.example.administrator.myapplication.activity;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -256,6 +259,28 @@ public class ExamActivity extends AppCompatActivity {
     public void nextExam(View view) {
         saveUserAnswer();
         showExam(biz.nextQuestion());
+    }
+
+    public void commit(View view) {
+        saveUserAnswer();
+        int s=biz.commitExam();
+        View inflate=View .inflate(this,R.layout.result,null);
+        TextView tvResult=(TextView)inflate.findViewById(R.id.tv_result);
+         tvResult.setText("你的分数\n"+s+"分！");
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setIcon(R.mipmap.exam_commit32x32)
+                .setTitle("交卷")
+                //.setMessage("你的分数为\n"+s+"分！")
+                .setView(inflate)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    finish();
+
+                    }
+                });
+        builder.create().show();
+
     }
 
     class LoadExamBroadcast extends BroadcastReceiver {
