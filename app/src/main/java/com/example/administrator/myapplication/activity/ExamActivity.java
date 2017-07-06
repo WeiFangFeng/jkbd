cbed3f9eb3a7182f094a4a27320dd7649b6b41dd
Potatos45 <2561024377@qq.com>
2017-07-06 08:49:58 +0800

    选择答案后直接给出结果，答案不可修改

--- app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/activity/ExamActivity.java
@@ -282,11 +282,18 @@ public class ExamActivity extends AppCompatActivity {
             if(userAnswer!=null && userAnswer.equals("")){
                 int userCB=Integer.parseInt(userAnswer)-1;
                 cbs[userCB].setChecked(true);
+                setOptions(true);
 
             }
+            else{setOptions(false);}
         }
     }
+    private void setOptions(boolean hasAnswer){
+        for (CheckBox cb : cbs) {
+            cb.setEnabled(!hasAnswer);
 
+        }
+    }
     private void resetOptions() {
         for (CheckBox cb: cbs) {
             cb.setChecked(false);
